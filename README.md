2432dc824f2e4bec9667ce36cd217b3748a09897
Andrew Peng <andrewpeng00@gmail.com>
2023-09-07 15:51:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installing for Chrome
+
+- Install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
+- View the raw source of the script [here]().
+- Click the install button.
+
+If a new Tampermonkey installtion tab did not appear, you can copy the source and paste it as a new userscript on the Tampermonkey dashboard.
---
+++ insights-unblur.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         Soundcloud Insights Unblur
+// @description  Unblurs Insights list elements
+// @version      0.1
+// @author       You
+// @match        https://insights-ui.soundcloud.com/*
+// @grant        none
+// ==/UserScript==
+
+// from https://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
+var observeDOM = (function(){
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  
+    return function( obj, callback ){
+      if( !obj || obj.nodeType !== 1 ) return;
+  
+      if( MutationObserver ){
+        // define a new observer
+        var mutationObserver = new MutationObserver(callback)
+  
+        // have the observer observe for changes in children
+        mutationObserver.observe( obj, { childList:true, subtree:true })
+        return mutationObserver
+      }
+  
+      // browser support fallback
+      else if( window.addEventListener ){
+        obj.addEventListener('DOMNodeInserted', callback, false)
+        obj.addEventListener('DOMNodeRemoved', callback, false)
+      }
+    }
+  })()
+  
+  const unblur = () => {
+      const listElements = document.getElementsByTagName("li");
+      for (let i = 0; i < listElements.length; i++) {
+          if (listElements[i].className.includes("Blur")) {
+              listElements[i].className = listElements[i].className.replace(/[0-9A-Za-z_]*Blur[0-9A-Za-z_]*/, "");
+          }
+      }
+  }
+  
+  const body = document.querySelector("body");
+  observeDOM(body, unblur);
