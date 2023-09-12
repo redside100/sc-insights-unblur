b80cdb95a3159c9aedce1174d8d210ef6b0a7a0d
Andrew Peng <andrewpeng00@gmail.com>
2023-09-12 13:18:28 -0400

    format

--- insights-unblur.user.js
+++ insights-unblur.user.js
@@ -8,37 +8,37 @@
 // ==/UserScript==
 
 // from https://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
-var observeDOM = (function(){
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-  
-    return function( obj, callback ){
-      if( !obj || obj.nodeType !== 1 ) return;
-  
-      if( MutationObserver ){
-        // define a new observer
-        var mutationObserver = new MutationObserver(callback)
-  
-        // have the observer observe for changes in children
-        mutationObserver.observe( obj, { childList:true, subtree:true })
-        return mutationObserver
-      }
-  
-      // browser support fallback
-      else if( window.addEventListener ){
-        obj.addEventListener('DOMNodeInserted', callback, false)
-        obj.addEventListener('DOMNodeRemoved', callback, false)
-      }
+var observeDOM = (function () {
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  return function (obj, callback) {
+    if (!obj || obj.nodeType !== 1) return;
+
+    if (MutationObserver) {
+      // define a new observer
+      var mutationObserver = new MutationObserver(callback)
+
+      // have the observer observe for changes in children
+      mutationObserver.observe(obj, { childList: true, subtree: true })
+      return mutationObserver
     }
-  })()
-  
-  const unblur = () => {
-      const listElements = document.getElementsByTagName("li");
-      for (let i = 0; i < listElements.length; i++) {
-          if (listElements[i].className.includes("Blur")) {
-              listElements[i].className = listElements[i].className.replace(/[0-9A-Za-z_]*Blur[0-9A-Za-z_]*/, "");
-          }
-      }
+
+    // browser support fallback
+    else if (window.addEventListener) {
+      obj.addEventListener('DOMNodeInserted', callback, false)
+      obj.addEventListener('DOMNodeRemoved', callback, false)
+    }
   }
-  
-  const body = document.querySelector("body");
-  observeDOM(body, unblur);
+})()
+
+const unblur = () => {
+  const listElements = document.getElementsByTagName("li");
+  for (let i = 0; i < listElements.length; i++) {
+    if (listElements[i].className.includes("Blur")) {
+      listElements[i].className = listElements[i].className.replace(/[0-9A-Za-z_]*Blur[0-9A-Za-z_]*/, "");
+    }
+  }
+}
+
+const body = document.querySelector("body");
+observeDOM(body, unblur);
